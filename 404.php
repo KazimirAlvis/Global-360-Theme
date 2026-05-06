5ae72e592608c1822623c36a6856025b31783276
KazimirAlvis <superkore.mail@gmail.com>
2026-05-06 11:43:20 -0700

    Refine 404 page layout and content

--- 404.php
+++ 404.php
@@ -10,51 +10,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main site-main-404">
+	<section class="error-404 not-found">
+		<header class="page-header">
+			<h1 class="page-title">
+				<span class="error-code">404</span>
+				<span class="error-title">Page Not Found</span>
+			</h1>
+		</header>
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'global-360-theme' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'global-360-theme' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'global-360-theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$global_360_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'global-360-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$global_360_theme_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+		<div class="page-content">
+			<p>We're sorry, the page you requested could not be found. Please go back to the homepage.</p>
+			<a class="btn btn_green" href="<?php echo esc_url( home_url( '/' ) ); ?>">Go Home</a>
+		</div>
+	</section>
+</main>
 
 <?php
 get_footer();
